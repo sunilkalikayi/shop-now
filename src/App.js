0566aef608a4bdbfa394107419266e529a4587fa
kalikayiCode <sunilkalikayiofficial@gmail.com>
2022-12-09 12:34:11 +0530

    navbar component is pushed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-
+import React from "react";
+import Navbar from "./components/navbar/Navbar"
 function App() {
   return (
-    <div>hellow ecommerse</div>
+    <div>
+     <Navbar/> 
+    </div>
      );
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import Brand from "../../assets/icons/brand.svg";
+import User from "../../assets/icons/user icon.svg";
+import Cart from "../../assets/icons/cart.svg";
+import Menu from "../../assets/icons/menu.svg";
+
+import './Navbar.css'
 
 export default function Navbar() {
-  return (
-    <div>
-      this is nav bar
-    </div>
-  )
+    return (
+        <div className="nav_bar">
+            <div className="nav_part1">
+                <img
+                    className="Brand_logo"
+                    src={Brand}
+                    alt="it is a brand logo"
+                ></img>
+                <div className="nav_items">
+                    <div className="nav_item">HOME</div>
+                    <div className="nav_item">ABOUT</div>
+                    <div className="nav_item">CONTACT US</div>
+                </div>
+            </div>
+            <div className="nav_part2">
+                <img src={User} alt="user icon"  />
+                <img src={Cart} alt="shopping cart icon"  />
+                <img src={Menu} alt="menu icon"  />
+            </div>
+        </div>
+    );
 }
